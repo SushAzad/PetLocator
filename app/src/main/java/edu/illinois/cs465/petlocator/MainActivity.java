3fe29032de0f3c0c21d9b26a51e90e4b17731142
Sushmita <azad.sushmita@gmail.com>
2018-11-18 10:08:11 -0600

    Google Marker with Sydney default

--- app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
@@ -1,13 +1,29 @@
 package edu.illinois.cs465.petlocator;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button gMapsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gMapsButton = (Button) findViewById(R.id.gmaps_button);
+        gMapsButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.gmaps_button) {
+            Intent intent = new Intent(MainActivity.this, LostPetLocationActivity.class);
+            startActivity(intent);
+        }
     }
 }
