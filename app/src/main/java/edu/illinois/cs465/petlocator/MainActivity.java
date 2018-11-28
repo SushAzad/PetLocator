2d1e4604c5ea5956ead74f17b03bf133b30118d3
Junsup Lee <Junsup@wirelessprv-10-192-219-203.near.illinois.edu>
2018-11-28 17:00:38 -0600

    addition

--- app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
@@ -2,11 +2,10 @@ package edu.illinois.cs465.petlocator;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -14,6 +13,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private Button petDetailButton;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
--- app/src/main/java/edu/illinois/cs465/petlocator/pet_details.java
+++ app/src/main/java/edu/illinois/cs465/petlocator/pet_details.java
@@ -2,6 +2,8 @@ package edu.illinois.cs465.petlocator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -16,6 +18,14 @@ public class pet_details extends Activity {
         Float longitude = getIntent().getFloatExtra("LostPetLongitude",0);
         TextView txt = findViewById(R.id.positionDetails);
         txt.setText("Pet lost at latitude: "+latitude);
+
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.breedDropDown);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(pet_details.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
 
     }
 }
