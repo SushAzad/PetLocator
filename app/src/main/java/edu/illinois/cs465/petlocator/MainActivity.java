a01b24ce50a1768ac326c1c009a08f517a636e91
Ryan Lee <jlee651@illinois.edu>
2018-11-28 00:59:26 -0600

    add

--- app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/petlocator/MainActivity.java
@@ -1,13 +1,30 @@
 package edu.illinois.cs465.petlocator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnContextClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity2();
+            }
+        };
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
